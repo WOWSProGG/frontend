09a16bd6e7594732b9cd310e8a461cbd1f81db7d
Kapil Viren Ahuja <k.v.ahuja@gmail.com>
2021-05-06 18:30:56 +0530

    initial commit. setup project. adding coming soon and having a base structure. responsive and mobile 1st is pending

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/Global.css";
+import "./styles/App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="flexContainer content csContent">
+            <header className="flexItem header">
+                <div className="flexContainer csHeaderCont">
+                    <img
+                        className="flexItem csItem logo"
+                        src="/assets/img/brand/logo_wows_legends.png"
+                        alt="Logo for World of Warships Stats"
+                    ></img>
+                    <p className="flexItem text">World of Warships: Statistics</p>
+                </div>
+            </header>
+
+            <section className="flexContainer csBannerCont">
+                <div className="flexItem csBanner">
+                    <img
+                        className="bannerImage"
+                        src="./assets/img/banners/WoWSL_Mass.jpg"
+                        alt="Massachussettes from the December 2020 campaign, with a NYC and Libery in background."
+                    />
+                </div>
+                <div class="flexItem csHeadingGroup">
+                    <h1 className="csHeading csHeading-1">Coming Soon</h1>
+                    <h2 className="csHeading csHeading-2">Just getting started, it will take a while!</h2>
+                </div>
+            </section>
+        </div>
+    );
 }
 
 export default App;
