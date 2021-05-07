94bec35b3a2d6134ed9540a201e9919f74377edb
Kapil Viren Ahuja <k.v.ahuja@gmail.com>
2021-05-08 00:41:02 +0530

    ready to build home page using bootstrap for grid

--- src/App.js
+++ src/App.js
@@ -2,23 +2,10 @@
 import { Container, Row, Col } from "react-bootstrap";
 
 // internal
-import { ComingSoon } from "./pages/ComingSoon";
 import { HomePage } from "./pages/HomePage";
 
 function App() {
-    return <ComingSoon />;
-    // return <HomePage />;
+    return <HomePage />;
 }
 
 export default App;
-
-//TODO: to be removed. these exist for reference
-// import "bootstrap/dist/css/bootstrap.min.css";
-
-// <Container>
-// <Row lg={4}>
-//     <Col>1 of 3</Col>
-//     <Col>2 of 3</Col>
-//     <Col>3 of 3</Col>
-// </Row>
-// </Container>
